5ff5b07ab0236682b38d624bbd7665c4fcc639c1
ezliu <evanliu@stanford.edu>
2018-05-20 21:49:06 -0700

    Figure 2b

--- main.py
+++ main.py
@@ -6,32 +6,63 @@ from tqdm import tqdm
 from util import aspl_lower_bound
 
 # Figure 1b)
-N = 40
-flow = range(1, N + 1)
-np.random.shuffle(flow)
-degrees = range(3, 32)
-observed = []
-lower_bound = []
+def figure1b():
+    N = 40
+    degrees = range(3, 32)
+    observed = []
+    lower_bound = []
 
-for degree in tqdm(degrees):
-    theoretical_bound = aspl_lower_bound(degree, N)
-    random_graph = Graph.rrg(N, degree)
+    for degree in tqdm(degrees):
+        theoretical_bound = aspl_lower_bound(degree, N)
+        random_graph = Graph.rrg(N, degree)
 
-    empirical = 0
-    for i in xrange(1, N + 1):
-        for j in xrange(i + 1, N + 1):
-            path = random_graph.k_shortest_paths(1, i, j)[0]
-            empirical += len(path) - 1
-    empirical = float(empirical) / (N * (N - 1) / 2.)
-    observed.append(empirical)
-    lower_bound.append(theoretical_bound)
-    print "Degree: {}".format(degree)
-    print "Observed ASLP: {}".format(empirical)
-    print "ASPL lower-bound: {}".format(theoretical_bound)
+        empirical = 0
+        for i in xrange(1, N + 1):
+            for j in xrange(i + 1, N + 1):
+                path = random_graph.k_shortest_paths(1, i, j)[0]
+                empirical += len(path) - 1
+        empirical = float(empirical) / (N * (N - 1) / 2.)
+        observed.append(empirical)
+        lower_bound.append(theoretical_bound)
+        print "Degree: {}".format(degree)
+        print "Observed ASLP: {}".format(empirical)
+        print "ASPL lower-bound: {}".format(theoretical_bound)
 
-plt.plot(degrees, observed, label="Observed ASPL")
-plt.plot(degrees, lower_bound, label="ASPL lower-bound")
-plt.xlabel("Network Degree")
-plt.ylabel("Path Length")
-plt.legend()
-plt.savefig("figure1b.png")
+    plt.plot(degrees, observed, label="Observed ASPL")
+    plt.plot(degrees, lower_bound, label="ASPL lower-bound")
+    plt.xlabel("Network Degree")
+    plt.ylabel("Path Length")
+    plt.legend()
+    plt.savefig("figure1b.png")
+#figure1b()
+
+# Figure 2b)
+def figure2b():
+    Ns = range(15, 200, 10)
+    degree = 10
+    observed = []
+    lower_bound = []
+
+    for N in Ns:
+        theoretical_bound = aspl_lower_bound(degree, N)
+        random_graph = Graph.rrg(N, degree)
+
+        empirical = 0
+        for i in xrange(1, N + 1):
+            for j in xrange(i + 1, N + 1):
+                path = random_graph.k_shortest_paths(1, i, j)[0]
+                empirical += len(path) - 1
+        empirical = float(empirical) / (N * (N - 1) / 2.)
+        observed.append(empirical)
+        lower_bound.append(theoretical_bound)
+        print "N: {}".format(N)
+        print "Observed ASLP: {}".format(empirical)
+        print "ASPL lower-bound: {}".format(theoretical_bound)
+
+    plt.plot(Ns, observed, label="Observed ASPL")
+    plt.plot(Ns, lower_bound, label="ASPL lower-bound")
+    plt.xlabel("Network Size")
+    plt.ylabel("Path Length")
+    plt.legend()
+    plt.savefig("figure2b.png")
+figure2b()
