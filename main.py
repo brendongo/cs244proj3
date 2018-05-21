514910d468862524b915ff402a4ef4864c7edca9
ezliu <evanliu@stanford.edu>
2018-05-20 21:37:48 -0700

    Add figure1b

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ from util import aspl_lower_bound
 N = 40
 flow = range(1, N + 1)
 np.random.shuffle(flow)
-for degree in tqdm(xrange(2, 32)):
+degrees = range(3, 32)
+observed = []
+lower_bound = []
+
+for degree in tqdm(degrees):
     theoretical_bound = aspl_lower_bound(degree, N)
     random_graph = Graph.rrg(N, degree)
 
@@ -19,6 +23,15 @@ for degree in tqdm(xrange(2, 32)):
             path = random_graph.k_shortest_paths(1, i, j)[0]
             empirical += len(path) - 1
     empirical = float(empirical) / (N * (N - 1) / 2.)
+    observed.append(empirical)
+    lower_bound.append(theoretical_bound)
     print "Degree: {}".format(degree)
     print "Observed ASLP: {}".format(empirical)
     print "ASPL lower-bound: {}".format(theoretical_bound)
+
+plt.plot(degrees, observed, label="Observed ASPL")
+plt.plot(degrees, lower_bound, label="ASPL lower-bound")
+plt.xlabel("Network Degree")
+plt.ylabel("Path Length")
+plt.legend()
+plt.savefig("figure1b.png")
